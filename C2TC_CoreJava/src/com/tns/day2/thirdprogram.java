47d98d8511278b3a251975e0d96381dc37a37196
akshy <akshy@System>
2024-09-04 14:36:41 +0530

    hi hello

---
+++ C2TC_CoreJava/src/com/tns/day2/thirdprogram.java
@@ -0,0 +1,23 @@
+package com.tns.day2;
+
+public class thirdprogram {
+	    public static void main(String[] args) {
+	        
+	        for (int i = 1; i <= 5; i++) {
+	            System.out.println("Number: " + i);
+	        }
+	  // while loop
+        int i = 1;
+    
+        while (i <= 5) {
+            System.out.println("Number: " + i);
+            i++;
+        }
+   // do while 
+        int j= 1;
+                do {
+            System.out.println("Number: " + i);
+            j++;
+        } while (j <= 5);
+    }
+}
